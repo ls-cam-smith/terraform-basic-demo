1c7406ca140df8433bebfd7a1b5a43a0a4eed314
Cam Smith <CameronL.Smith@countdown.co.nz>
2022-09-29 11:19:05 +1300

    Add some _slightly_ messy HCL code.

--- terraform.tf
+++ terraform.tf
@@ -5,3 +5,19 @@ resource "random_pet" "demo" {
 output "pet_name" {
   value = random_pet.demo.id
 }
+
+data "http" "quote" {
+  url = "https://animechan.vercel.app/api/random"
+
+  lifecycle {
+    postcondition {
+      condition = contains([200], self.status_code)
+      error_message = "Status code invalid"
+    }
+  }
+}
+
+resource "local_file" "quote_file" {
+  filename = "anime_quote.txt"
+  content = jsondecode(data.http.quote.response_body)["quote"]
+}
